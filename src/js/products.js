c852f262eb29ff63071ba96a6c4337464057e86f
h-genie <ses2000hj@hanmail.net>
2024-11-23 17:46:17 +0900

    feat : 초기 작업세팅

---
+++ src/js/products.js
@@ -0,0 +1,18 @@
+class Product {
+  constructor(name, price) {
+    this.name = name
+    this.price = price
+  }
+}
+
+const products = [
+  new Product("콜라", 1500),
+  new Product("속이사이다", 1700),
+  new Product("판타지판타", 1500),
+  new Product("오뎅국물", 1800),
+  new Product("부장라떼", 800),
+  new Product("판타지판타2", 1500),
+  new Product("레드뿔", 2500),
+  new Product("핫세븐", 1900),
+  new Product("커피우유", 1400)
+]
