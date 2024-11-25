1813a32fe6c262860e4be95a1d05566f40aa5164
Fabbro <fabbro@infograb.net>
2024-11-25 20:20:03 +0900

    feat: 불필요한 코드 삭제, css 스타일 유지

--- src/main.js
+++ src/main.js
@@ -1,8 +1,1 @@
 import "./style.css";
-import { createRoot } from "./core/createRoot.js";
-
-const root = createRoot(document.getElementById("app"));
-
-document.addEventListener("DOMContentLoaded", () => {
-  root.render(<div>hello world</div>);
-});
