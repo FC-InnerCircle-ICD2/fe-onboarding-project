943b906e9270ef9ef770373e98bef73ce9daad83
Leeyoungryul <lekjkj@naver.com>
2024-11-25 20:32:50 +0900

    feat: 기초 화면 구성

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 import './style.css'
 
 
-document.querySelector('#app').innerHTML = `
-  <div>
-  
-  </div>
-`
+
+const products_list = [ "쿨라", "속이사이다", "판타지판타", "오뎅국물",
+    "부장라떼", "판타지판타", "레드뿔", "핫세븐", "커피우유"];
+const prices_list = [1500, 1700, 1500, 1800, 800, 1500, 2500, 1900, 1400]
+
+const products = document.querySelectorAll(".product");
+const prices = document.querySelectorAll(".price");
+
+for (let i = 0; i < products.length; i++) {
+    products[i].innerText = products_list[i];
+}
+
+for (let i = 0; i < prices.length; i++) {
+    prices[i].innerText = `${prices_list[i]}원`;
+}
 
