be56e31999d971b02c6cffdb3508dd4c4534aba8
Fabbro <fabbro@infograb.net>
2024-11-25 21:15:07 +0900

    feat: 현재 잔액 표시 기능 추가

---
+++ src/VendingMachine.js
@@ -0,0 +1,9 @@
+class VendingMachine {
+  constructor() {
+    this.currentMoney = 0;
+    this.insertedMoney = 0;
+    this.logs = [];
+  }
+}
+
+export default VendingMachine;
--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
 import "./style.css";
+import VendingMachine from "./VendingMachine";
+
+// 자판기 생성
+const vendingMachine1 = new VendingMachine();
+
+/********** 엘리멘트 요소들 **********/
+const currentMoneyEl = document.querySelector('[aria-label="현재 잔액"]');
+
+/********** 기능 구현 **********/
+// 현재 잔액 표시
+currentMoneyEl.textContent = vendingMachine1.currentMoney;
