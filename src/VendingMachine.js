f5626c33f05d196fc608dd4ba019d096910bc918
Fabbro <fabbro@infograb.net>
2024-11-25 21:43:08 +0900

    feat: 투입 기능 구현

--- src/VendingMachine.js
+++ src/VendingMachine.js
@@ -4,6 +4,19 @@ class VendingMachine {
     this.insertedMoney = 0;
     this.logs = [];
   }
+
+  /**
+   * @param {string} money - 투입된 금액
+   */
+  insertMoney(money) {
+    const parsedMoney = parseInt(money);
+    if (parsedMoney > 0) {
+      this.currentMoney += parsedMoney;
+      this.logs.push(`${parsedMoney}원을 투입했습니다.`);
+    }
+    const insertedMoneyEl = document.querySelector('[aria-label="투입 금액"]');
+    insertedMoneyEl.value = 0;
+  }
 }
 
 export default VendingMachine;
--- src/main.js
+++ src/main.js
@@ -6,7 +6,19 @@ const vendingMachine1 = new VendingMachine();
 
 /********** 엘리멘트 요소들 **********/
 const currentMoneyEl = document.querySelector('[aria-label="현재 잔액"]');
+const insertedMoneyInputEl = document.querySelector('[aria-label="투입 금액"]');
+const insertButtonEl = document.querySelector('[aria-label="투입"]');
+const logsEl = document.querySelector('[aria-label="로그"]');
 
 /********** 기능 구현 **********/
 // 현재 잔액 표시
 currentMoneyEl.textContent = vendingMachine1.currentMoney;
+// 로그 표시
+logsEl.textContent = vendingMachine1.logs.join("\n");
+
+// 투입 버튼 클릭
+insertButtonEl.addEventListener("click", () => {
+  vendingMachine1.insertMoney(insertedMoneyInputEl.value);
+  currentMoneyEl.textContent = vendingMachine1.currentMoney;
+  logsEl.textContent = vendingMachine1.logs.join("\n");
+});
