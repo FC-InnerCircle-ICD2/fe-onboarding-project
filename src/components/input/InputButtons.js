c5f60b4f05121971065f6f299c9dbb8aaae3e52f
김지훈 <kigpand@gmail.com>
2024-11-25 21:41:30 +0900

    자판기 버튼 클릭시 해당 메뉴 가격 저장

--- src/components/input/InputButtons.js
+++ src/components/input/InputButtons.js
@@ -1,8 +1,9 @@
 import { inputInfo } from "../../utils/inputInfo.js";
 
 export default class InputButtons {
-  constructor(parent) {
+  constructor(parent, setPrice) {
     this.parent = parent;
+    this.setPrice = setPrice;
     this.buttons = document.createElement("div");
     this.buttons.classList.add("inputField_buttons");
   }
@@ -19,7 +20,11 @@ export default class InputButtons {
       const price = document.createElement("div");
       price.innerText = item.price;
       info.appendChild(price);
+      info.value = item.price;
 
+      info.addEventListener("click", (e) => {
+        this.setPrice(e.currentTarget.value);
+      });
       this.buttons.appendChild(info);
     });
     this.parent.appendChild(this.buttons);
--- src/components/input/InputField.js
+++ src/components/input/InputField.js
@@ -3,13 +3,19 @@ import InputButtons from "./InputButtons.js";
 export default class InputField {
   constructor(parent) {
     this.parent = parent;
+    this.price = 0;
     this.inputField = document.createElement("div");
     this.inputField.classList.add("inputField");
   }
 
+  setPrice(price) {
+    this.price = price;
+  }
+
   init() {
-    const buttons = new InputButtons(this.inputField);
+    const buttons = new InputButtons(this.inputField, this.setPrice);
     buttons.init();
+
     this.parent.appendChild(this.inputField);
   }
 }
