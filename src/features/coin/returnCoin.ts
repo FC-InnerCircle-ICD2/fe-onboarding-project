9584174a1a18c1ba2c122c2cea00b92601e7ec39
HyoungMin <hyoungqu23@gmail.com>
2024-11-27 01:48:36 +0900

    생성된 CoinController 인스턴스를 활용해 Coin 반환 로직 `returnCoin`를 구현한다

---
+++ src/features/coin/returnCoin.ts
@@ -0,0 +1,8 @@
+import type { CoinController } from '../../entities/coin';
+import { formatCurrency } from '../../shared/currency';
+
+export const returnCoin = (coinController: CoinController) => {
+  const returnedCoin = coinController.returnCoin();
+
+  alert(`${formatCurrency(returnedCoin)}원이 반환되었습니다.`);
+};
