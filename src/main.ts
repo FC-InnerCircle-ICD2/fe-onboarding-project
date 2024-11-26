5a27e0025d2f7d01422dd03966cc70e93cc775b2
HyoungMin <hyoungqu23@gmail.com>
2024-11-27 01:46:41 +0900

    생성된 ProductController, CoinController의 인스턴스를 생성한다

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,5 @@
+import { CoinController } from './entities/coin';
+import { ProductController } from './entities/products';
 import './style.css';
+const productController = new ProductController();
+const coinController = new CoinController();
