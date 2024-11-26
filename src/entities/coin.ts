14fe2486d25e321013f83e81883e3f4ca61f2286
HyoungMin <hyoungqu23@gmail.com>
2024-11-27 01:45:08 +0900

    Coin Entity에 CoinController 클래스를 선언한다

--- src/entities/coin.ts
+++ src/entities/coin.ts
@@ -1,1 +1,31 @@
+import { TProduct } from './products';
+
 export type TCoin = number;
+
+export class CoinController {
+  private balance: TCoin = 0;
+
+  getCoin() {
+    return this.balance;
+  }
+
+  insertCoin(newCoin: TCoin) {
+    if (newCoin < 0) {
+      alert('잘못된 투입 금액입니다.');
+      return;
+    }
+
+    this.balance += newCoin;
+  }
+
+  useCoin(price: TProduct['price']) {
+    this.balance -= price;
+  }
+
+  returnCoin() {
+    const currentCoin = this.balance;
+    this.balance = 0;
+
+    return currentCoin;
+  }
+}
