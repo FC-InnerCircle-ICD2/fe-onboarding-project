5d581f7d1d2dd015e7e8da0470c3b883faa6b8fd
HyoungMin <hyoungqu23@gmail.com>
2024-11-27 01:47:42 +0900

    생성된 ProductController, CoinController 인스턴스를 활용해 Product 구매 로직 `purchaseProduct`를 구현한다

---
+++ src/features/products/purchaseProduct.ts
@@ -0,0 +1,26 @@
+import type { CoinController } from '../../entities/coin';
+import type { ProductController, TProduct } from '../../entities/products';
+import { formatCurrency } from '../../shared/currency';
+import type { TResponse } from '../../shared/response';
+
+export const purchaseProduct = (
+  product: TProduct,
+  productController: ProductController,
+  coinController: CoinController,
+): TResponse => {
+  const currentCoin = coinController.getCoin();
+
+  if (product.price <= currentCoin) {
+    productController.setProduct(product);
+
+    coinController.useCoin(product.price);
+
+    return { ok: true };
+  } else {
+    const diff = Math.abs(currentCoin - product.price);
+
+    alert(`투입한 금액이 ${formatCurrency(diff)}원 부족합니다.`);
+
+    return { ok: false };
+  }
+};
