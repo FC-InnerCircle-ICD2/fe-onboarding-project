00c4f312a0ae90c58758068813f2acf66827bc6f
HelloHailie <alsltjsals@gmail.com>
2024-11-26 23:56:17 +0900

    Refactor: 데이터는 products 파일에서 관리하도록 수정

--- src/main.js
+++ src/main.js
@@ -1,22 +1,5 @@
 import "./style.css";
-
-const PRODUCTS = [
-  { id: 1, name: "콜라", price: 1500 },
-  { id: 2, name: "사이다", price: 1500 },
-  { id: 3, name: "환타", price: 1500 },
-  { id: 4, name: "밀키스", price: 1800 },
-  { id: 5, name: "커피", price: 2000 },
-  { id: 6, name: "물", price: 1000 },
-  { id: 7, name: "이온음료", price: 2000 },
-  { id: 8, name: "주스", price: 1800 },
-  { id: 9, name: "에너지드링크", price: 2500 },
-];
-
-const VENDING_MACHINE_CONFIG = {
-  TITLE: "음료 자판기",
-  MONEY_PLACEHOLDER: "금액 입력",
-  LOG_TITLE: "작동 로그",
-};
+import { PRODUCTS, VENDING_MACHINE_CONFIG } from "./data/products.js";
 
 const state = {
   currentMoney: 0,
