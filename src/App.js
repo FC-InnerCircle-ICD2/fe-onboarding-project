2f561022487b38285d7f57efcd72491fd9ff5a98
seokhochoi <seokho9473@gmail.com>
2024-11-26 22:55:03 +0900

    feat: 엔트리 파일 설정

--- src/App.js
+++ src/App.js
@@ -1,1 +1,17 @@
+import Component from './core/Component.js';
+import VendingMachine from './components/VendingMachine.js';
+
 /** Main application component */
+export default class App extends Component {
+  template() {
+    return `
+    <div data-component="vending-machine"></div>
+    `;
+  }
+
+  mounted() {
+    new VendingMachine(
+      this.$target.querySelector('[data-component="vending-machine"]')
+    );
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
+import App from './App.js';
+
 /** Entry point */
+const rootElement = document.querySelector('#app');
+if (rootElement) {
+  new App(rootElement);
+} else {
+  console.error('Root element 404 or HTMLDivElement를 찾지 못했습니다.');
+}
