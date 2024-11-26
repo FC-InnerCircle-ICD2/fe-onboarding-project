9b808c1fbb00b26d48ef6a34cfb557c7d3f44eb0
김지훈 <kigpand@gmail.com>
2024-11-26 21:55:06 +0900

    자판기 음료 버튼 클릭 동작 구현

--- src/components/input/InputFieldButton.js
+++ src/components/input/InputFieldButton.js
@@ -10,11 +10,25 @@ export default class InputFieldButton {
     this.element.appendChild(labelTag);
 
     const priceTag = document.createElement("div");
-    priceTag.innerText = price;
+    priceTag.innerText = price + "원";
     this.element.appendChild(priceTag);
     this.element.value = price;
+    this.element.dataset.label = label;
 
     // 클릭시 구매 이벤트 요청
-    this.element.addEventListener("click", (e) => {});
+    this.element.addEventListener("click", (e) => {
+      const inputText = document.querySelector(
+        `.inputField_inputText.${version}`
+      );
+      const inputNum = Number(inputText.innerText.replaceAll(",", ""));
+      const value = Number(e.currentTarget.value);
+      if (inputNum >= value) {
+        const outputTextarea = document.querySelector(
+          `.outputField_textarea.${version}`
+        );
+        outputTextarea.innerHTML += `${e.currentTarget.dataset.label}을 구매했습니다.<br />`;
+        inputText.innerText = (inputNum - value).toLocaleString();
+      }
+    });
   }
 }
