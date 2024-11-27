dba867554875a43d56033e0c0b89d75978167396
HyoungMin <hyoungqu23@gmail.com>
2024-11-27 20:08:12 +0900

    상품 버튼을 생성하는 createProductButton 로직을 추가한다

--- src/widgets/products/productButton.ts
+++ src/widgets/products/productButton.ts
@@ -4,6 +4,22 @@ import type {
   TProduct,
 } from '../../entities/products/model';
 import { purchaseProduct } from '../../features/products/purchaseProduct';
+import { formatCurrency } from '../../shared/currency';
+import { updateProductWindow } from './productWindow';
+
+export const createProductButton = (product: TProduct) => {
+  const button = document.createElement('button');
+
+  button.id = product.id;
+  button.className = 'product-button';
+  button.innerHTML = `
+    <p class='product-button_name'>${product.name}</p>
+    <span class='product-button_price'>${formatCurrency(product.price)}원</span>
+    `;
+
+  return button;
+};
+
 export const handleProductButtonClick = (
   product: TProduct,
   productController: ProductController,
