41806fb3e8b73affdfe165d312352942f51a84fa
HyoungMin <hyoungqu23@gmail.com>
2024-11-27 21:35:35 +0900

    로그 서비스 인스턴스를 생성한다

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import { CoinController } from './entities/coin/model';
 import { ProductController } from './entities/products/model';
+import { LogService } from './shared/log';
 import './style.css';
 import { createInsertCoinButton } from './widgets/coin/coinInsertButton';
 import { createReturnCoinButton } from './widgets/coin/coinReturnButton';
 import { createProductButtons } from './widgets/products/productButtons';
 import { updateProductWindow } from './widgets/products/productWindow';
 
+const logWindowElement = document.querySelector<HTMLDivElement>('.log-window');
+
 const productController = new ProductController();
 const coinController = new CoinController();
+const logService = new LogService(logWindowElement!);
 
 document.addEventListener('DOMContentLoaded', () => {
   updateProductWindow(0);
