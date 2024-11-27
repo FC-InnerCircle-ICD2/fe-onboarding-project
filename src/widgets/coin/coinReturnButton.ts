a679303bdaede9a4248bf2ba9e5e4a30e85afff1
HyoungMin <hyoungqu23@gmail.com>
2024-11-27 20:04:46 +0900

    Coin 반환 버튼을 추가하는 createReturnCoinButton을 생성한다

--- src/widgets/coin/coinReturnButton.ts
+++ src/widgets/coin/coinReturnButton.ts
@@ -1,5 +1,20 @@
 import type { CoinController } from '../../entities/coin/model';
 import { returnCoin } from '../../features/coin/returnCoin';
+import { updateProductWindow } from '../products/productWindow';
+
+const coinReturnButtonElement = document.querySelector<HTMLButtonElement>(
+  '.coin-return-button',
+);
+
+export const createReturnCoinButton = (coinController: CoinController) => {
+  coinReturnButtonElement!.addEventListener('click', () => {
+    handleReturnCoinButtonClick(coinController);
+
+    const currentBalance = coinController.getCoin();
+
+    updateProductWindow(currentBalance);
+  });
+};
 
 export const handleReturnCoinButtonClick = (coinController: CoinController) => {
   returnCoin(coinController);
