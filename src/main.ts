8e4f67f6f68ab470410cc4a18f01a23fb59acd3c
HyoungMin <hyoungqu23@gmail.com>
2024-11-27 20:10:43 +0900

    DOM 로드 후 UI를 렌더링한다

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
 import { CoinController } from './entities/coin/model';
 import { ProductController } from './entities/products/model';
 import './style.css';
+import { createInsertCoinButton } from './widgets/coin/coinInsertButton';
+import { createReturnCoinButton } from './widgets/coin/coinReturnButton';
+import { createProductButtons } from './widgets/products/productButtons';
+import { updateProductWindow } from './widgets/products/productWindow';
+
 const productController = new ProductController();
 const coinController = new CoinController();
+
+document.addEventListener('DOMContentLoaded', () => {
+  updateProductWindow(0);
+  createProductButtons(productController, coinController);
+  createInsertCoinButton(coinController);
+  createReturnCoinButton(coinController);
+});
