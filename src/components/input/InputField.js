38a68c3990a9c02ba3a3b2ce5356261840691e24
김지훈 <kigpand@gmail.com>
2024-11-28 22:42:37 +0900

    inputfield 이벤트 위임

--- src/components/input/InputField.js
+++ src/components/input/InputField.js
@@ -20,6 +20,43 @@ export default class InputField {
         new InputFieldButton(this.version, item.label, item.price).element
       );
     });
+
+    buttons.addEventListener("mousedown", (e) => {
+      if (e.target.className === "inputField_buttons") return;
+      const inputText = document.querySelector(
+        `.inputField_inputText.${this.version}`
+      );
+      const inputNum = Number(inputText.innerText.replaceAll(",", ""));
+      const value = Number(e.target.value);
+      if (inputNum >= value) {
+        const outputTextarea = document.querySelector(
+          `.outputField_textarea.${this.version}`
+        );
+
+        outputTextarea.innerHTML += `${e.target.dataset.label}을 구매했습니다.<br />`;
+        outputTextarea.scrollTo({
+          top: outputTextarea.scrollHeight,
+          behavior: "smooth",
+        });
+
+        inputText.innerText = (inputNum - value).toLocaleString();
+        inputText.dataset.value = inputNum - value;
+      } else {
+        inputText.innerText = value.toLocaleString();
+      }
+    });
+
+    buttons.addEventListener("mouseup", (e) => {
+      if (e.target.className === "inputField_buttons") return;
+      const inputText = document.querySelector(
+        `.inputField_inputText.${this.version}`
+      );
+      const inputNum = Number(inputText.dataset.value);
+      const value = Number(e.target.value);
+      if (value > inputNum) {
+        inputText.innerText = Number(inputText.dataset.value).toLocaleString();
+      }
+    });
     return buttons;
   }
 }
--- src/components/input/InputFieldButton.js
+++ src/components/input/InputFieldButton.js
@@ -1,53 +1,9 @@
 export default class InputFieldButton {
   constructor(version, label, price) {
-    // inputField_buttons_button
-
     this.element = document.createElement("button");
     this.element.className = `inputField_buttons_button ${version}`;
     this.element.value = price;
     this.element.dataset.label = label;
-
-    const labelTag = document.createElement("div");
-    labelTag.innerText = label;
-    this.element.appendChild(labelTag);
-
-    const priceTag = document.createElement("div");
-    priceTag.innerText = price + "원";
-    this.element.appendChild(priceTag);
-
-    this.element.addEventListener("mousedown", (e) => {
-      const inputText = document.querySelector(
-        `.inputField_inputText.${version}`
-      );
-      const inputNum = Number(inputText.innerText.replaceAll(",", ""));
-      const value = Number(e.currentTarget.value);
-      if (inputNum >= value) {
-        const outputTextarea = document.querySelector(
-          `.outputField_textarea.${version}`
-        );
-
-        outputTextarea.innerHTML += `${e.currentTarget.dataset.label}을 구매했습니다.<br />`;
-        outputTextarea.scrollTo({
-          top: outputTextarea.scrollHeight,
-          behavior: "smooth",
-        });
-
-        inputText.innerText = (inputNum - value).toLocaleString();
-        inputText.dataset.value = inputNum - value;
-      } else {
-        inputText.innerText = value.toLocaleString();
-      }
-    });
-
-    this.element.addEventListener("mouseup", (e) => {
-      const inputText = document.querySelector(
-        `.inputField_inputText.${version}`
-      );
-      const inputNum = Number(inputText.dataset.value);
-      const value = Number(e.currentTarget.value);
-      if (value > inputNum) {
-        inputText.innerText = Number(inputText.dataset.value).toLocaleString();
-      }
-    });
+    this.element.innerHTML = `${price}원`;
   }
 }
