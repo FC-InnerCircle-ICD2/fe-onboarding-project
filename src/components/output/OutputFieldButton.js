1594741eefdaf4aa029546480e388acbb6fbe247
김지훈 <kigpand@gmail.com>
2024-11-28 22:35:11 +0900

    outputfield 이벤트 위임

--- src/components/output/OutputFieldButton.js
+++ src/components/output/OutputFieldButton.js
@@ -1,64 +1,9 @@
 export default class OutputFieldButton {
-  constructor(text, version) {
+  constructor(text) {
     this.element = document.createElement("button");
     this.element.innerText = text;
     this.element.className = `outputField_buttons_button ${
       text === "투입" ? "insert" : "return"
     }`;
-    this.element.addEventListener("click", () => {
-      if (text === "투입") {
-        this.handleInsertEvent(version);
-      } else {
-        this.handleReturnEvent(version);
-      }
-    });
-  }
-
-  changeOutputTextarea(version, value) {
-    const outputTextarea = document.querySelector(
-      `.outputField_textarea.${version}`
-    );
-    outputTextarea.innerHTML += value;
-    outputTextarea.scrollTo({
-      top: outputTextarea.scrollHeight,
-      behavior: "smooth",
-    });
-  }
-
-  handleInsertEvent(version) {
-    const outputText = document.querySelector(
-      `.outputField_inputText.${version}`
-    );
-    const inputText = document.querySelector(
-      `.inputField_inputText.${version}`
-    );
-
-    if (outputText.value !== "0" && outputText.value !== "") {
-      this.changeOutputTextarea(
-        version,
-        `${outputText.value}원이 투입되었습니다.<br />`
-      );
-      inputText.innerText = (
-        Number(inputText.innerText.replaceAll(",", "")) +
-        Number(outputText.value)
-      ).toLocaleString();
-      inputText.dataset.value =
-        Number(inputText.dataset.value) + Number(outputText.value);
-      outputText.value = "";
-    }
-  }
-
-  handleReturnEvent(version) {
-    const inputText = document.querySelector(
-      `.inputField_inputText.${version}`
-    );
-    if (inputText.innerText !== "0") {
-      this.changeOutputTextarea(
-        version,
-        `${inputText.innerText.replaceAll(",", "")}원이 반환되었습니다.<br />`
-      );
-      inputText.innerText = "0";
-      inputText.dataset.value = "0";
-    }
   }
 }
--- src/components/output/OutputFieldButtons.js
+++ src/components/output/OutputFieldButtons.js
@@ -6,7 +6,59 @@ export default class OutputFieldButtons {
     this.element = document.createElement("div");
     this.element.className = "outputField_buttons";
 
-    this.element.appendChild(new OutputFieldButton("투입", version).element);
-    this.element.appendChild(new OutputFieldButton("반환", version).element);
+    this.element.appendChild(new OutputFieldButton("투입").element);
+    this.element.appendChild(new OutputFieldButton("반환").element);
+
+    this.element.addEventListener("click", (e) => {
+      const inputText = document.querySelector(
+        `.inputField_inputText.${version}`
+      );
+      if (e.target.innerText === "투입") {
+        this.handleInsertEvent(inputText, version);
+      } else {
+        this.handleReturnEvent(inputText, version);
+      }
+    });
+  }
+
+  changeOutputTextarea(version, value) {
+    const outputTextarea = document.querySelector(
+      `.outputField_textarea.${version}`
+    );
+    outputTextarea.innerHTML += value;
+    outputTextarea.scrollTo({
+      top: outputTextarea.scrollHeight,
+      behavior: "smooth",
+    });
+  }
+
+  handleInsertEvent(inputText, version) {
+    const outputText = document.querySelector(
+      `.outputField_inputText.${version}`
+    );
+    if (outputText.value !== "0" && outputText.value !== "") {
+      this.changeOutputTextarea(
+        version,
+        `${outputText.value}원이 투입되었습니다.<br />`
+      );
+      inputText.innerText = (
+        Number(inputText.innerText.replaceAll(",", "")) +
+        Number(outputText.value)
+      ).toLocaleString();
+      inputText.dataset.value =
+        Number(inputText.dataset.value) + Number(outputText.value);
+      outputText.value = "";
+    }
+  }
+
+  handleReturnEvent(inputText, version) {
+    if (inputText.innerText !== "0") {
+      this.changeOutputTextarea(
+        version,
+        `${inputText.innerText.replaceAll(",", "")}원이 반환되었습니다.<br />`
+      );
+      inputText.innerText = "0";
+      inputText.dataset.value = "0";
+    }
   }
 }
