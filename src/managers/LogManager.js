f84469e2e7e9a61db4bd89cee7c610da3ad1c2c7
Fabbro <fabbro@infograb.net>
2024-11-30 10:43:17 +0900

    refactor: LogManager 클래스에 private 키워드 적용
    - logs와 logsContainerEl을 private 필드로 변경
    - render메서드를 private로 변경

--- src/managers/LogManager.js
+++ src/managers/LogManager.js
@@ -1,23 +1,26 @@
 class LogManager {
+  #logs;
+  #logsContainerEl;
+
   constructor(machineEl) {
     /** @type {string[]} */
-    this.logs = [];
+    this.#logs = [];
     /** @type {HTMLElement} */
-    this.logsContainerEl = machineEl.querySelector('[aria-label="로그"]');
+    this.#logsContainerEl = machineEl.querySelector('[aria-label="로그"]');
   }
 
   add(log) {
-    this.logs.push(log);
-    this.render();
+    this.#logs.push(log);
+    this.#render();
   }
 
-  render() {
+  #render() {
     // 로그 요소 리셋
-    while (this.logsContainerEl.firstChild) {
-      this.logsContainerEl.removeChild(this.logsContainerEl.firstChild);
+    while (this.#logsContainerEl.firstChild) {
+      this.#logsContainerEl.removeChild(this.#logsContainerEl.firstChild);
     }
 
-    this.logs.forEach((log) => {
+    this.#logs.forEach((log) => {
       const logEl = document.createElement("div");
       logEl.textContent = log;
       const logColor = log.includes("실패")
@@ -26,10 +29,10 @@ class LogManager {
         ? "text-emerald-600"
         : "text-black";
       logEl.classList.add(logColor);
-      this.logsContainerEl.appendChild(logEl);
+      this.#logsContainerEl.appendChild(logEl);
     });
-    this.logsContainerEl.scrollTo({
-      top: this.logsContainerEl.scrollHeight,
+    this.#logsContainerEl.scrollTo({
+      top: this.#logsContainerEl.scrollHeight,
       behavior: "smooth",
     });
   }
