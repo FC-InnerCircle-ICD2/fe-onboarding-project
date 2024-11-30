d3db80cb63c6cd9283e3dd37a74a0a670d05d8af
Fabbro <fabbro@infograb.net>
2024-11-30 10:45:34 +0900

    refactor: private 필드와 메서드 적용
    - 클래스 내부 필드들을 private으로 변경
    - 내부 메서드들을 private으로 변경
    - init() 메서드는 public 유지

--- src/managers/ProductButtonManager.js
+++ src/managers/ProductButtonManager.js
@@ -1,20 +1,25 @@
 class ProductButtonManager {
+  #machineEl;
+  #products;
+  #handlers;
+  #productButtonListEl;
+
   /**
    * @param {HTMLElement} machineEl - 자판기 DOM 요소
    * @param {Product[]} products - 상품 목록
    * @param {Object} handlers - 이벤트 핸들러 객체
    */
   constructor(machineEl, products, handlers) {
-    this.machineEl = machineEl;
-    this.products = products;
-    this.handlers = handlers;
-    this.productButtonListEl = this.machineEl.querySelector(
+    this.#machineEl = machineEl;
+    this.#products = products;
+    this.#handlers = handlers;
+    this.#productButtonListEl = this.#machineEl.querySelector(
       ".product-button-list"
     );
   }
 
-  createProductButtons() {
-    this.products.forEach((product) => {
+  #createProductButtons() {
+    this.#products.forEach((product) => {
       const button = document.createElement("button");
       const productNameEl = document.createElement("span");
       const productPriceEl = document.createElement("span");
@@ -40,43 +45,40 @@ class ProductButtonManager {
 
       button.appendChild(productNameEl);
       button.appendChild(productPriceEl);
-      this.productButtonListEl.appendChild(button);
+      this.#productButtonListEl.appendChild(button);
     });
   }
 
-  initEventListeners() {
-    // 버튼 클릭 이벤트 추가
-    this.productButtonListEl.addEventListener("click", (e) => {
+  #initEventListeners() {
+    this.#productButtonListEl.addEventListener("click", (e) => {
       const buttonEl = e.target.closest("button");
       if (!buttonEl) return;
 
       const productName = buttonEl.getAttribute("data-name");
-      const targetProduct = this.products.find(
+      const targetProduct = this.#products.find(
         (product) => product.name === productName
       );
       if (targetProduct) {
-        this.handlers.onBuyProduct(targetProduct);
+        this.#handlers.onBuyProduct(targetProduct);
       }
     });
 
-    // 잔액 부족시 버튼 누르고 있을 때 이벤트
-    this.productButtonListEl.addEventListener("mousedown", (e) => {
+    this.#productButtonListEl.addEventListener("mousedown", (e) => {
       const buttonEl = e.target.closest("button");
       if (!buttonEl) return;
 
       const productPrice = parseInt(buttonEl.getAttribute("data-price"));
-      this.handlers.onProductMouseDown(productPrice);
+      this.#handlers.onProductMouseDown(productPrice);
     });
 
-    // 버튼에서 손을 뗄 때 이벤트
-    this.productButtonListEl.addEventListener("mouseup", () => {
-      this.handlers.onProductMouseUp();
+    this.#productButtonListEl.addEventListener("mouseup", () => {
+      this.#handlers.onProductMouseUp();
     });
   }
 
   init() {
-    this.createProductButtons();
-    this.initEventListeners();
+    this.#createProductButtons();
+    this.#initEventListeners();
   }
 }
 
