f04128482f70d0b826aed4352de49e86ff3244ec
Fabbro <fabbro@infograb.net>
2024-11-30 18:00:23 +0900

    feat: 반환 기능 관련 테스트 코드 추가

--- src/test/vendingMachine.test.js
+++ src/test/vendingMachine.test.js
@@ -9,7 +9,7 @@ describe("VendingMachine", () => {
 
   beforeEach(() => {
     // 테스트 자판기 DOM 요소 생성
-    const { machine, displayPanel, input, insertButton } =
+    const { machine, displayPanel, input, insertButton, returnButton } =
       createVendingMachineDOM(1);
     document.body.appendChild(machine);
 
@@ -21,7 +21,7 @@ describe("VendingMachine", () => {
       displayPanel.textContent = balanceManager.getBalance().toLocaleString();
     };
 
-    // insertMoney
+    /******* insertMoney (start) *******/
     const insertMoney = () => {
       const parsedMoney = parseInt(input.value.replace(/,/g, ""));
       if (parsedMoney > 0) {
@@ -42,6 +42,19 @@ describe("VendingMachine", () => {
         insertMoney();
       }
     });
+    /******* insertMoney (end) *******/
+
+    /******* returnChange (start) *******/
+    const returnChange = () => {
+      const returnedChange = balanceManager.returnChange();
+      if (returnedChange === 0) return;
+      logManager.add(`[잔액 반환] ${returnedChange.toLocaleString()}원`);
+      updateBalanceDisplay();
+    };
+
+    // 잔액 반환 버튼 클릭 이벤트 핸들러
+    returnButton.addEventListener("click", returnChange);
+    /******* returnChange (end) *******/
 
     testElements = {
       displayPanel,
@@ -49,6 +62,7 @@ describe("VendingMachine", () => {
       insertButton,
       balanceManager,
       logManager,
+      returnButton,
     };
   });
 
@@ -97,6 +111,37 @@ describe("VendingMachine", () => {
       input.dispatchEvent(enterEvent);
 
       expect(balanceManager.getBalance()).toBe(2000);
+    });
+  });
+
+  describe("잔액 반환(returnChange) 테스트", () => {
+    beforeEach(() => {
+      // 1000원을 투입하고 시작
+      const { input, insertButton } = testElements;
+      input.value = "1000";
+      insertButton.click();
+    });
+
+    it("잔액을 반환하고 잔액이 0으로 초기화되는 것을 확인", () => {
+      const { balanceManager, returnButton } = testElements;
+      returnButton.click();
+      expect(balanceManager.getBalance()).toBe(0);
+    });
+
+    it("로그에 잔액 반환 내역이 추가되는 것을 확인", () => {
+      const { returnButton } = testElements;
+      returnButton.click();
+      expect(testElements.logManager.getLogs().at(-1)).toContain(
+        "[잔액 반환] 1,000원"
+      );
+    });
+
+    it("잔액이 0원이면 잔액 반환 버튼을 눌러도 로그가 추가되지 않는 것을 확인", () => {
+      const { returnButton } = testElements;
+      returnButton.click();
+      returnButton.click();
+      // 초기 투입 1회 + 잔액 반환 1회 + 잔액 0원 로그 추가하지 않음으로 0회 = 총 2회
+      expect(testElements.logManager.getLogs().length).toBe(2);
     });
   });
 });
