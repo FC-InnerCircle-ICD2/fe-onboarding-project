bf4e6316bb6b9ef7115324f2d54d21f9cd0c5467
Fabbro <fabbro@infograb.net>
2024-11-30 10:42:06 +0900

    refactor: CurrentBalanceManager 클래스에 private 필드 추가
    - balance을 private 필드로 변경

--- src/managers/CurrentBalanceManager.js
+++ src/managers/CurrentBalanceManager.js
@@ -1,41 +1,39 @@
 class CurrentBalanceManager {
-  constructor() {
-    this.balance = 0;
-  }
+  #balance = 0;
 
   getBalance() {
-    return this.balance;
+    return this.#balance;
   }
 
   add(money) {
     if (money > 0) {
-      this.balance += money;
+      this.#balance += money;
     }
-    return this.balance;
+    return this.#balance;
   }
 
   subtract(money) {
     if (this.canAfford(money)) {
-      this.balance -= money;
+      this.#balance -= money;
       return true;
     }
     return false;
   }
 
   returnChange() {
-    const previousBalance = this.balance;
-    this.balance = 0;
+    const previousBalance = this.#balance;
+    this.#balance = 0;
     return previousBalance;
   }
 
   // 잔액(balance)이 상품가격(price)보다 크거나 같은지 확인
   canAfford(price) {
-    return this.balance >= price;
+    return this.#balance >= price;
   }
 
   // 잔액(balance)이 상품가격(price)보다 부족한 금액 계산
   getNeededMoney(price) {
-    return (this.balance - price) * -1;
+    return (this.#balance - price) * -1;
   }
 }
 
