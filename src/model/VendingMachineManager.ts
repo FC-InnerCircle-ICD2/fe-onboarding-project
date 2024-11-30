621751cada01dfbcb967278d18ad8ad8cd16b6ec
jinsewoon-dev <jinsewoon.dev@gmail.com>
2024-11-30 10:40:00 +0900

    레거시 소스 삭제

--- src/model/VendingMachineManager.ts
+++ src/model/VendingMachineManager.ts
@@ -234,28 +234,3 @@ export class VendingMachineManager {
     this.updateState();
   }
 }
-
-export class Product {
-  private _disabled: boolean;
-  private _item: HTMLButtonElement;
-  constructor() {
-    this._disabled = false;
-    this._item = document.createElement("button");
-  }
-  get disabled() {
-    return this._disabled;
-  }
-  set disabled(value: boolean) {
-    this._disabled = value;
-  }
-
-  get item() {
-    return this._item;
-  }
-  setItemState({ disabled = false }: { disabled?: boolean }) {
-    this._item.disabled = disabled;
-  }
-  innerHTML(innerHTML: string) {
-    this._item.innerHTML = innerHTML;
-  }
-}
