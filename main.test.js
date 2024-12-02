c48cd729d35999962b381454b1e96c132ba81a1a
김지훈 <kigpand@gmail.com>
2024-12-02 22:28:58 +0900

    테스트 setting 및 code 추가

--- main.test.js
+++ main.test.js
@@ -1,10 +1,29 @@
-import { expect, test } from "vitest";
 import Machine from "./src/components/Machine";
+import { beforeEach, describe, expect, it } from "vitest";
+import { fireEvent, screen } from "@testing-library/dom";
 
-test("machine test", () => {
-  const machine = new Machine();
-  machine.on();
+describe("machine", () => {
+  let version = "";
+  let machine;
+  let price;
 
-  const machineId = document.querySelector(".machine");
-  expect(machineId).not.toBeNull();
+  beforeEach(() => {
+    version = "one";
+    machine = new Machine(version);
+    price = "1700원";
+  });
+
+  it("머신 버전 on 테스트", () => {
+    machine.on();
+
+    const machineId = document.querySelector(`.${version}`);
+    expect(machineId).not.toBeNull();
+  });
+
+  it("Press the machine button", async () => {
+    machine.on();
+
+    const div = screen.getAllByText(price);
+    fireEvent.click(div[1]);
+  });
 });
