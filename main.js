2649d6cbce18bdac9acba600cfe379e27387119b
Leeyoungryul <lekjkj@naver.com>
2024-12-02 20:50:33 +0900

    refactor: 상품 버튼 통일 및 로그 함수 생성

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
 import './style.css'
 
 const screen = document.querySelector("#screen");
-const itemBtns = document.querySelectorAll(".btn");
-const inputBtn = document.querySelector("#in_btn");
-const inMoney = document.querySelector("#in_money");
-const moneyLog = document.querySelector("#money_log");
-const outBtn = document.querySelector("#out_btn");
+const inputBtn = document.querySelector("#inBtn");
+const inMoney = document.querySelector("#inMoney");
+
 
 let money = 0;
 let logs = [];
+screen.value = 0;
+inMoney.value = 0;
 
+
+function Log (text) {
+    const moneyLog = document.querySelector("#moneyLog");
+    logs.push(text);
+    moneyLog.value = logs.join("\n");
+    moneyLog.scrollTop = moneyLog.scrollHeight;
+    screen.value = Number(money).toLocaleString();
+}
 
 
 const itemList = [
@@ -29,48 +37,34 @@ const itemList = [
 itemList.forEach(item => {
     const template = document.querySelector('#items');
     const clone = document.importNode(template.content, true);
+    const button = clone.querySelector('#btn');
     const productDiv = clone.querySelector('#product');
     const priceDiv = clone.querySelector('#price');
 
     productDiv.textContent = item.name;
     priceDiv.textContent = `${item.price} 원`;
 
-    template.appendChild(clone);
-});
 
-
-
-screen.value = 0;
-inMoney.value = 0;
-
-
-// 상품 버튼 클릭 이벤트
-itemBtns.forEach(button => {
     button.addEventListener('click', () => {
-        const price = button.querySelector('.price').textContent.replace(/[^0-9]/g, "");
-        const product = button.querySelector('.product').textContent;
-
-        if(money < price) {
-            screen.value = Number(price).toLocaleString();
+        if(money < item.price) {
+            screen.value = Number(priceDiv.textContent).toLocaleString();
 
             setTimeout(function () {
-                logs.push(`돈이 부족합니다.`);
-                moneyLog.value = logs.join("\n");
-                moneyLog.scrollTop = money_log.scrollHeight;
-                screen.value = Number(money).toLocaleString();
+                Log(`돈이 부족합니다.`)
 
             },1000);
 
         }
         else {
-            money = money - price;
-            screen.value = Number(money).toLocaleString();
-            logs.push(`${product}을 구매했습니다.`);
-            moneyLog.value = logs.join("\n");
-            moneyLog.scrollTop = moneyLog.scrollHeight;
+            money = money - item.price;
+            Log(`${item.name}을 구매했습니다.`)
         }
     });
+
+    template.appendChild(clone);
 });
+
+
 
 
 // 돈 투입 버튼
@@ -78,23 +72,14 @@ inputBtn.addEventListener('click', () => {
     const result = inMoney.value.replace(/,/g, '')
     money =  money + parseInt(result);
 
-    screen.value = Number(money).toLocaleString();
-
-    logs.push(`${result}원을 투입했습니다.`);
-    moneyLog.value = logs.join("\n");
-
-    moneyLog.scrollTop = moneyLog.scrollHeight;
+    Log(`${result}원을 투입했습니다.`)
 
     inMoney.value = 0;
 });
 
 
-// 돈 반환 버튼 (0으로 초기화)
-outBtn.addEventListener('click', () => {
-    logs.push(`${money}원을 반환합니다.`);
-    moneyLog.value = logs.join("\n");
-
-    moneyLog.scrollTop = moneyLog.scrollHeight;
+document.querySelector("#outMoneyBtn").addEventListener('click', () => {
+    Log(`${money}원을 반환합니다.`)
 
     money = 0;
     screen.value = money;
